cd5dd3e00e0c22c386e5536ab1eeae651a8213fd
maikondouglas <maikonconta0@gmail.com>
2021-01-12 03:35:56 -0300

    Adding #authenticate_with_token! method

--- app/controllers/concerns/authenticable.rb
+++ app/controllers/concerns/authenticable.rb
@@ -1,5 +1,9 @@
 module Authenticable
   def current_user
-    @current_user ||= @User.find_by(auth_token: request.headers['Authorization'])
+    @current_user ||= User.find_by(auth_token: request.headers['Authorization'])
+  end
+
+  def authenticate_with_token!
+    render json: { errors: 'Unauthorized acess!' }, status: 401 unless current_user.present?
   end
 end

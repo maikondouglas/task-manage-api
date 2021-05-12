56964353795454c300e3e9681f92fd8d956eb18e
maikondouglas <maikonconta0@gmail.com>
2021-05-12 01:47:37 -0300

    Adding Ransack for tasks

--- spec/requests/api/v2/tasks_spec.rb
+++ spec/requests/api/v2/tasks_spec.rb
@@ -13,17 +13,36 @@ RSpec.describe 'Task API' do
   end
 
   describe 'GET /tasks' do
-    before do
-      create_list(:task, 5, user_id: user.id)
-      get '/tasks', params: {}, headers: headers
+    context 'when no filter param is sent' do
+      before do
+        create_list(:task, 5, user_id: user.id)
+        get '/tasks', params: {}, headers: headers
+      end
+
+      it 'returns status code 200' do
+        expect(response).to have_http_status(200)
+      end
+
+      it 'returns 5 tasks from database' do
+        expect(json_body[:data].count).to eq(5)
+      end
     end
 
-    it 'returns status code 200' do
-      expect(response).to have_http_status(200)
-    end
+    context 'when fiter params is sent' do
+      let!(:notebook_task_1) { create(:task, title: 'Check if the notebook is broken', user_id: user.id) }
+      let!(:notebook_task_2) { create(:task, title: 'Buy a new notebook', user_id: user.id) }
+      let!(:other_task_1)    { create(:task, title: 'Fix the door', user_id: user.id) }
+      let!(:other_task_2)    { create(:task, title: 'Buy a new car', user_id: user.id) }
 
-    it 'returns 5 tasks from database' do
-      expect(json_body[:data].count).to eq(5)
+      before do
+        get '/tasks?q[title_cont]=note', params: {}, headers: headers
+      end
+
+      it 'returns only the tasks matching' do
+        returned_task_titles = json_body[:data].map { |t| t[:attributes][:title] }
+
+        expect(returned_task_titles).to eq([notebook_task_1.title, notebook_task_2.title])
+      end
     end
   end
 
@@ -33,7 +52,7 @@ RSpec.describe 'Task API' do
     before { get "/tasks/#{task.id}", params: {}, headers: headers }
 
     it 'returns status code 200' do
-      expect(response).to have_http_status(200) 
+      expect(response).to have_http_status(200)
     end
 
     it 'returns the json for task' do
@@ -130,11 +149,11 @@ RSpec.describe 'Task API' do
     end
 
     it 'returns status code 204' do
-      expect(response).to have_http_status(204) 
+      expect(response).to have_http_status(204)
     end
 
     it 'removes the task from the database' do
-      expect { Task.find(task.id) }.to raise_error(ActiveRecord::RecordNotFound) 
+      expect { Task.find(task.id) }.to raise_error(ActiveRecord::RecordNotFound)
     end
   end
 end
